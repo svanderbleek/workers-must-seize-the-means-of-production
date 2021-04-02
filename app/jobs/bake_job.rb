77ae25a00f3a2e3ac8e58f99ca1a3e93580e5727
svanderbleek <sandy.vanderbleek@gmail.com>
2021-04-02 17:53:14 -0400

    debugging test queue

--- app/jobs/bake_job.rb
+++ app/jobs/bake_job.rb
@@ -1,6 +1,8 @@
 class BakeJob < ApplicationJob
   queue_as :default
 
+  TIME = 4.seconds
+
   def perform(oven)
     oven.bake!
   end
--- spec/features/cooking/cookies_spec.rb
+++ spec/features/cooking/cookies_spec.rb
@@ -14,7 +14,9 @@ feature 'Cooking cookies' do
 
     expect(current_path).to eq(oven_path(oven))
     expect(page).to have_content 'Chocolate Chip'
-    expect(page).to have_content 'Your Cookie is Ready'
+    expect(page).to_not have_content 'Your Cookie is Ready'
+
+    binding.pry
 
     click_button 'Retrieve Cookie'
     expect(page).to_not have_content 'Chocolate Chip'
